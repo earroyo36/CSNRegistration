9db95b69bc2bef9008693361676d7917ed418c37
tiredautocorrect <lexeve07@gmail.com>
2025-04-29 19:30:25 -0700

    Update views.py
    tweak (please work)

--- CsnWeb/CSNdjango/ExamWeb/views.py
+++ CsnWeb/CSNdjango/ExamWeb/views.py
@@ -17,33 +17,38 @@ def RegisterView(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
         role = 'student'  # 👈 hardcode for now
+        valid = False
 
        if user is not None:
             if user.email.endswith('@student.csn.edu') or user.email.endswith('@csn.edu'):
          #       register(request, user)
-            else:
-                messages.error(request, "Only CSN emails are allowed.")
+            valid = True
+
+        if valid = True:
+        
+            if CustomUser.objects.filter(username=email).exists():
+                messages.error(request, "Email already exists.")
                 return redirect('register')
-                
-        if CustomUser.objects.filter(username=email).exists():
-            messages.error(request, "Email already exists.")
+    
+            if len(password) < 8:
+                messages.error(request, "Password must be at least 8 characters.")
+                return redirect('register')
+    
+            user = CustomUser.objects.create_user(
+                username=email,
+                email=email,
+                first_name=first_name,
+                last_name=last_name,
+                password=password,
+                role=role
+            )
+    
+            messages.success(request, "Account created successfully. Please log in.")
+            return redirect('login')
+        
+        else:
+            messages.error(request, "Only CSN emails are allowed.")
             return redirect('register')
-
-        if len(password) < 8:
-            messages.error(request, "Password must be at least 8 characters.")
-            return redirect('register')
-
-        user = CustomUser.objects.create_user(
-            username=email,
-            email=email,
-            first_name=first_name,
-            last_name=last_name,
-            password=password,
-            role=role
-        )
-
-        messages.success(request, "Account created successfully. Please log in.")
-        return redirect('login')
 
     return render(request, 'register.html')
 
